5ad3cc1b8683013c0e1589969d3f6ff7423dee4a
“Debora <debora.crescenzo@gmail.com>
2020-10-29 18:57:32 +0000

    test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 require("./setup/index")(app);
 
 //Routers definition
+app.get("/", (req, res) => res.json(true));
 app.use("/auth", require("./routes/auth.route"));
 app.use("/user", require("./routes/user.route"));
 app.use("/comm", require("./routes/comm.route"));
