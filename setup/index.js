afc3568a21de7d2b4a5812bc40877456d8a3a343
“Debora <debora.crescenzo@gmail.com>
2020-11-11 10:10:07 +0000

    debug

--- setup/index.js
+++ setup/index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const favicon = require("serve-favicon");
 
 module.exports = (app) => {
   app.use(logger("dev"));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(cookieParser());
-  app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
   //CORS configuration
   app.use(
     cors({
